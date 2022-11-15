e162295a29cfeae8fed3b95283de6c659ea24d3d
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-11-15 20:50:38 +0100

    add app component style and test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import "./App.css";
+import { useState } from "react";
+import Description from "./components/Description/Description";
+import Form from "./components/Form/Form";
+import Results from "./components/Results/Results";
 
-function App() {
+const initialCheckArray: number[] = [];
+
+const App = (): JSX.Element => {
+  const [checkResults, setCheckResult] = useState(initialCheckArray);
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className="main-title">Leadtech</h1>
+
+      <Description />
+      <Form checkResults={checkResults} setCheckResult={setCheckResult} />
+
+      <Results results={checkResults} />
     </div>
   );
-}
+};
 
 export default App;
