27a53065e2d3908136215ce6c0b8d97ff99c64da
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-11-15 16:32:10 +0100

    add function getRangeNumbers

---
+++ src/utils/getRangeNumbers.ts
@@ -0,0 +1,11 @@
+const getRangeNumbersArray = (min: number, max: number): number[] => {
+  const numbersRange = [];
+
+  for (let number = min; number <= max; number++) {
+    numbersRange.push(number);
+  }
+
+  return numbersRange;
+};
+
+export default getRangeNumbersArray;
